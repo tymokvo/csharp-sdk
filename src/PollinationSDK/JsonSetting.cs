f806773169101131bb398638fd9b4a93dc1deb3d
MingboPeng <mingo1214@gmail.com>
2020-07-13 11:52:12 -0400

    fix(Json): add JsonSetting for serializer

---
+++ src/PollinationSDK/JsonSetting.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace PollinationSDK.Model
+{
+
+    public static class JsonSetting
+    {
+		private static JsonSerializerSettings _setting;
+		public static JsonSerializerSettings ConvertSetting
+		{
+			get {
+
+				if (_setting == null)
+				{
+					_setting = new JsonSerializerSettings
+					{
+						NullValueHandling = NullValueHandling.Ignore,
+						MissingMemberHandling = MissingMemberHandling.Ignore
+					};
+				}
+				return _setting; 
+			}
+		}
+
+	}
+}
