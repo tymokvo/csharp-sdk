8a6971fc0deae3238c89da76282dbef31ab58f11
MingboPeng <mingo1214@gmail.com>
2020-07-21 16:47:08 -0400

    fix(CI): update version based on timestemp

--- .openapi-generator/update_assembly_version.py
+++ .openapi-generator/update_assembly_version.py
@@ -1,5 +1,8 @@
 import re, os, json
+from datetime import datetime
 
+now = datetime.now()
+deltaTime = (int)((now - datetime(now.year, 1, 1, 0, 0, 0)).total_seconds())
 
 
 config_file = os.path.join(os.getcwd(), '.openapi-generator', 'config.json')
@@ -7,13 +10,13 @@ config_file = os.path.join(os.getcwd(), '.openapi-generator', 'config.json')
 with open(config_file, "r") as jsonFile:
     config_data = json.load(jsonFile)
 
-schema_version = config_data["packageVersion"]
+schema_version = config_data["packageVersion"] + f'.{deltaTime}'
 
-assembly_file = os.path.join(os.getcwd(), 'src', 'PollinationSDK', 'Properties', 'AssemblyInfo.cs')
+assembly_file = os.path.join(os.getcwd(), 'src', 'PollinationSDK', 'PollinationSDK.csproj')
 
-with open(assembly_file, "r") as csFile:
+with open(assembly_file, encoding='utf-8', mode='r') as csFile:
     s = csFile.read()
-with open(assembly_file, 'w') as f:
-    s = re.sub(r"(?<=assembly: AssemblyVersion\(\")\S+(?=\"\))", schema_version + '.*', s)
+with open(assembly_file, encoding='utf-8', mode='w') as f:
+    s = re.sub(r"(?<=\SVersion\>)\S+(?=\<\/)", schema_version, s)
     print(f"Update AssemblyVersion to: {schema_version}")
     f.write(s)
