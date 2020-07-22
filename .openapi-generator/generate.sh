81c8201deaee6a5a22300d18ae63dce77f56cc59
MingboPeng <mingo1214@gmail.com>
2020-07-22 14:58:42 -0400

    fix(generator): remove generate_dev.sh

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-npx openapi-generator generate \
-  -i https://api.pollination.cloud/sdk_openapi.json \
-  -t .openapi-generator/templates/csharp \
-  -g csharp \
-  -o . \
-  --skip-validate-spec \
-  -c .openapi-generator/config.json
+python .openapi-generator/pre_gen_script.py 
+npx openapi-generator generate -i "https://api.pollination.cloud/sdk_openapi.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+python .openapi-generator/post_gen_script.py "https://api.pollination.cloud/sdk_openapi.json" 
+python .openapi-generator/update_assembly_version.py
--- .openapi-generator/generate_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This is the file used for local development
-
-python .openapi-generator/pre_gen_script.py 
-npx openapi-generator generate -i "https://api.pollination.cloud/sdk_openapi.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
-python .openapi-generator/post_gen_script.py "https://api.pollination.cloud/sdk_openapi.json" 
-python .openapi-generator/update_assembly_version.py
