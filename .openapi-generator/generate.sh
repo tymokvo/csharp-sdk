ffc8c29fe07f2b4978055f6fa2d809b60dc27899
Antoine Dao <antoinedao1@gmail.com>
2020-09-15 12:24:39 +0100

    build(openapi): point to the non-modded openapi endpoint

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python .openapi-generator/pre_gen_script.py 
-npx @openapitools/openapi-generator-cli generate -i "https://api.pollination.cloud/sdk_openapi.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
-python .openapi-generator/post_gen_script.py "https://api.pollination.cloud/sdk_openapi.json" 
+npx @openapitools/openapi-generator-cli generate -i "https://api.pollination.cloud/openapi.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+python .openapi-generator/post_gen_script.py "https://api.pollination.cloud/openapi.json" 
 python .openapi-generator/update_assembly_version.py
 
 
