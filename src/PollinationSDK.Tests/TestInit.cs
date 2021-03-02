001546c65a10ca377464fa508a82c6ad51493698
MingboPeng <mingo1214@gmail.com>
2021-03-02 18:46:38 -0500

    test(*): add api tests against the server

---
+++ src/PollinationSDK.Tests/TestInit.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PollinationSDK.Test
+{
+    [SetUpFixture]
+    public class TestInit
+    {
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            // environment
+            var key = Environment.GetEnvironmentVariable("PollinationApiKey");
+            if (string.IsNullOrEmpty(key))
+            {
+                // for local development tests, you must add Api key to ApiKey.txt
+                key = System.IO.File.ReadAllText(@"../../../ApiKey.txt");
+            }
+
+            var apiAuthentication = key;
+            AuthHelper.SignInWithApiAuthAsync(apiAuthentication, null, devEnv: true).Wait();
+        }
+    }
+}
