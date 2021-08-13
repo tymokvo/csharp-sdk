c9d4adced458a50077082eb058e048c9b875c3c6
MingboPeng <mingo1214@gmail.com>
2021-08-13 12:03:34 -0400

    fix(SortEnum): fix SortEnum

---
+++ src/PollinationSDK/Model/SortEnum.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace PollinationSDK
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+
+    public enum SortEnum
+    {
+        [EnumMember(Value = "ascending")]
+        Ascending = 1,
+
+        [EnumMember(Value = "descending")]
+        Descending = 2,
+
+
+    }
+
+}
